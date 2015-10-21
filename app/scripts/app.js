96443aa32bac3d44895a8ac55080337f4b324aa1
PrasadDammalapati <naveen.prasad152@gmail.com>
2015-10-21 23:44:36 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+﻿var myApp = angular.module("myApp", ["ngRoute", 'photogallery']);
+myApp.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+    .when("/", { templateUrl: 'views/mygallery.html', controller: 'myCtrl' })
+    .otherwise('/');
+}]);
+
+myApp.controller("myCtrl", function ($scope, $document) {
+    $scope.images = [
+      { thumb: 'images/thumbs/1.jpg', img: 'images/1.jpg', description: 'Beautiful Houses' },
+          { thumb: 'images/thumbs/2.jpg', img: 'images/2.jpg', description: 'Bed' },
+          { thumb: 'images/thumbs/3.jpg', img: 'images/3.jpg', description: 'Nature' },
+          { thumb: 'images/thumbs/4.jpg', img: 'images/4.jpg', description: 'Traditional Hall' },
+          { thumb: 'images/thumbs/5.jpg', img: 'images/5.jpg', description: 'New Model Bed' },
+          { thumb: 'images/thumbs/6.jpg', img: 'images/6.jpg', description: 'Way of life' }
+    ];
+});
